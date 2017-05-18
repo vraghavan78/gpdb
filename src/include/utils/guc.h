358a454ae136de8cc91c6c2bbfb5a344a4c87945
Venkatesh Raghavan <vraghavan@pivotal.io>
2017-05-17 17:02:18 -0700

    Make ICG tests pass when GPDB is compiled with disable-orca
    In the updated tests, we used functions like disable_xform and
    enable_xform to hint the optimizer to disallow/allow a particular
    physical node. However, these functions are only available when GPDB
    is built with GPORCA. Planner on the other hand accomplished this
    via a GUC.
    
    To avoid usage of these functions in tests, I have introduced couple
    of GUCS that mimic the same planner behavior but now for GPORCA.
    In this effort I needed to add an API inside GPORCA.
    
    https://github.com/greenplum-db/gporca/pull/177
    
    By using these gucs accompanied by the changes in the sql/out files the
    following tests now pass when we build GPDB with --disable-orca
    
    * bfv_catalog
    * bfv_partition
    * bitmapscan_ao
    * bitmapscan
    * indexjoin
    * join_gp
    * qp_left_anti_semi_join
    * co_nestloop_idxscan
    
    I am working on the remaining failing tests are:
    * bfv_index
    * bfv_statistic
    * gp_optimizer

--- src/include/utils/guc.h
+++ src/include/utils/guc.h
@@ -437,6 +437,9 @@ extern bool optimizer_enable_dml_triggers;
 extern bool	optimizer_enable_dml_constraints;
 extern bool optimizer_enable_direct_dispatch;
 extern bool optimizer_enable_master_only_queries;
+extern bool optimizer_enable_hashjoin;
+extern bool optimizer_enable_dynamic_table_scan;
+extern bool optimizer_enable_index_scan;
 
 /* Optimizer plan enumeration related GUCs */
 extern bool optimizer_enumerate_plans;
